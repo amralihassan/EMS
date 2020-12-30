653135f9899b00de5f5f7c63f9edfb8835cd91cf
amr <amr_dotnet@ymail.com>
2020-12-30 12:44:56 +0200

    create project layout

---
+++ resources/lang/en/local.php
@@ -0,0 +1,23 @@
+<?php
+return
+[
+    'dashboard'                     => 'Dashboard',
+    'users'                         => 'Users',
+    'add_product'                   => 'Add Product',
+    'manage_products'               => 'Manage Products',
+    'categories'                    => 'Categories',
+    'view_orders'                   => 'View Orders',
+    'payments'                      => 'Payments',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => '',
+    ''  => ''
+];
---
+++ resources/views/admin/layouts/nav.blade.php
@@ -0,0 +1,26 @@
+  <!-- BEGIN: Main Menu-->
+  <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
+    <div class="navbar-header">
+        <ul class="nav navbar-nav flex-row">
+            <li class="nav-item mr-auto"><a class="navbar-brand" href="{{route('dashboard')}}">
+                    <div class="brand-logo"></div>
+                    <h2 class="brand-text mb-0">Vuexy</h2>
+                </a></li>
+            <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
+        </ul>
+    </div>
+    <div class="shadow-bottom"></div>
+    <div class="main-menu-content">
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-home"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.dashboard') }}</span></a></li>
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-users"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.users') }}</span></a></li>
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-plus"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.add_product') }}</span></a></li>
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-edit"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.manage_products') }}</span></a></li>
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-grid"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.categories') }}</span></a></li>
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-shopping-cart"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.view_orders') }}</span></a></li>
+            <li class=" nav-item"><a href="app-email.html"><i class="feather icon-credit-card"></i><span class="menu-title" data-i18n="Dashboard">{{ trans('local.payments') }}</span></a></li>
+
+        </ul>
+    </div>
+</div>
+<!-- END: Main Menu-->
