c732087a4b2723026adf781f74b68168dd483033
amr <amr_dotnet@ymail.com>
2021-01-15 10:38:49 +0200

    created all controllers and requests for student settings

---
+++ app/Modules/student/Http/Controllers/Settings/ReportFormController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Student\Http\Controllers\Settings;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ReportFormController extends Controller
+{
+    //
+}
